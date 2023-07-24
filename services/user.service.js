edd87be1ab527ed6762619328aaae7afc81b8ae2
minaz99 <hanymina43@gmail.com>
2023-07-24 09:22:31 +0200

    adding express import

--- services/user.service.js
+++ services/user.service.js
@@ -1,7 +1,9 @@
 const db = require("../dbConfig");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-
+const express = require("express");
+const app = express();
+app.use(express.json());
 const userServices = {
   login: async (req, res) => {
     let { email, password } = req.body;
