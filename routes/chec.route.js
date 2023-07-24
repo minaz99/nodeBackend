20e6a6a1804633196f3f1b87556c141e1b6f2f8d
minaz99 <hanymina43@gmail.com>
2023-07-24 21:29:59 +0200

    contract get endpoint

--- routes/chec.route.js
+++ routes/chec.route.js
@@ -3,6 +3,17 @@ const router = express.Router();
 
 const cs = require("../services/chec.service");
 
-router.get("/", cs.def);
+function authToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
+router.get("/", cs.def);
+router.get("/contracts", authToken, cs.contract);
 module.exports = router;
--- services/chec.service.js
+++ services/chec.service.js
@@ -2,5 +2,8 @@ const trialService = {
   def: (req, res) => {
     res.json("Hello there");
   },
+  contract: (req, res) => {
+    res.json("Authorized");
+  },
 };
 module.exports = trialService;
