1128a7c8dacfe63c4193a50f47bcdd02773256f4
minaz99 <hanymina43@gmail.com>
2023-07-25 11:46:13 +0200

    removed unessary commented lines + authToken

--- routes/chec.route.js
+++ routes/chec.route.js
@@ -1,18 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
 const cs = require("../services/chec.service");
-
-function authToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-}
 
 router.get("/", cs.def);
 //router.get("/contracts", authToken, cs.contract);
