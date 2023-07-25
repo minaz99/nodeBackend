410655d9b527e7dc1fe45551ce7d5e99ea13243c
minaz99 <hanymina43@gmail.com>
2023-07-25 13:40:21 +0200

    endpoint for filter by stage to /contract/filter

--- routes/contract.route.js
+++ routes/contract.route.js
@@ -5,7 +5,7 @@ const authToken = require("../authToken");
 const contractService = require("../services/contract.service");
 router.get("/", authToken, contractService.getAllContracts);
 router.get("/:id", authToken, contractService.getContractByID);
-router.get("/:stage",authToken,contractService.getContractsByContractStage);
+router.get("/filter/:stage",authToken,contractService.getContractsByContractStage);
 router.post("/", authToken, contractService.createContract);
 
 module.exports = router;
