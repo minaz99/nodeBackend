81f0f42d849d761a1a2cbc43cdde3557c4f2179b
minaz99 <hanymina43@gmail.com>
2023-07-25 13:34:26 +0200

    changing response from rows -> rows[0]

--- services/contract.service.js
+++ services/contract.service.js
@@ -78,7 +78,7 @@ const contract = {
         `SELECT * FROM contracts where contractStage = $1`,
         [contractStage]
       );
-      res.status(200).json({ contracts: rows });
+      res.status(200).json({ contracts: rows[0] });
     } catch (err) {
       res.status(404).json({ error: err.msg });
     }
