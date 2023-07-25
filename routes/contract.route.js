377dac82e634cacc9936d209ba44d7c9a8c32b0d
minaz99 <hanymina43@gmail.com>
2023-07-25 11:46:28 +0200

    Added authToken as an imported function

--- routes/contract.route.js
+++ routes/contract.route.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const router = express.Router();
-function authToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-}
+const authToken = require("../authToken");
+
 const contractService = require("../services/contract.service");
 router.get("/", authToken, contractService.getAllContracts);
 router.get("/:id", authToken, contractService.getContractByID);
