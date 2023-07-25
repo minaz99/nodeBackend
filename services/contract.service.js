2533adf40fa75bc98e950b6c9a8191f42746fada
minaz99 <hanymina43@gmail.com>
2023-07-25 14:12:40 +0200

    dkjfndk

--- services/contract.service.js
+++ services/contract.service.js
@@ -73,11 +73,12 @@ const contract = {
   },
   getContractsByContractStage: async (req, res) => {
     try {
-      const { rows } = await db.query(
+      const { rowCount } = await db.query(
         `SELECT * FROM contracts where ContractStage = $1`,
         [req.query.stage]
       );
-      res.status(200).json({ filtered: rows });
+      if (rowCount > 0) res.status(200).json("Got it");
+      else res.status(404).json("Not found");
     } catch (err) {
       res.status(404).json({ error: err.msg });
     }
