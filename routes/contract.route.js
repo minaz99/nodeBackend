062d9221be71b32826adc90c9706c91155ff64be
minaz99 <hanymina43@gmail.com>
2023-07-27 21:36:49 +0200

    Removing patch for set photographer and video

--- routes/contract.route.js
+++ routes/contract.route.js
@@ -12,7 +12,6 @@ router.get(
 );
 router.get("/search/:bride", authToken, contractService.getContractByBride);
 router.post("/", authToken, contractService.createContract);
-router.patch("/:id", authToken, contractService.addCommentToContract);
-router.patch("/:id", authToken, contractService.setPhotographer);
-router.patch("/:id", authToken, contractService.setVideo);
+router.post("/:id", authToken, contractService.addCommentToContract);
+
 module.exports = router;
