abc2f9190e1c7c17d6960d7c965af8a196447c51
minaz99 <hanymina43@gmail.com>
2023-07-27 18:24:31 +0200

    asdas

--- services/contract.service.js
+++ services/contract.service.js
@@ -88,7 +88,7 @@ const contract = {
         `SELECT * FROM contracts WHERE brideName LIKE $1`,
         [`${req.params.bride}%`]
       );*/
-      res.json(200).json(req.params.bride);
+      res.json(req.params.bride);
     } catch (err) {
       res.json({ error: err });
     }
