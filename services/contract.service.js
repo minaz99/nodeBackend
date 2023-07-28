9521e3267ca39791f9a79445cece030bf8493bcd
minaz99 <hanymina43@gmail.com>
2023-07-29 00:08:36 +0200

    adasd

--- services/contract.service.js
+++ services/contract.service.js
@@ -204,7 +204,7 @@ const contract = {
         ].contracts.push(contract)
       );
       res.json({ Days: contractsMonthDetails });*/
-      res.json(`Bride: ${result.rows[0]}`);
+      res.json(`Bride: ${result.rows[0].eventDate}`);
     } catch (err) {
       res.status(400).json({ error: err.msg });
     }
