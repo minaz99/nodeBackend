ca9c58ec7076f8f0a6e3dc622fdce197310b9c59
minaz99 <hanymina43@gmail.com>
2023-07-29 01:07:59 +0200

    sdkfjnsd

--- services/contract.service.js
+++ services/contract.service.js
@@ -204,7 +204,10 @@ const contract = {
         ].contracts.push(contract)
       );
       res.json({ Days: contractsMonthDetails });*/
-      res.json(result.rows[0].eventDate);
+      for (let r of result.rows) {
+        console.log(r.eventDate);
+      }
+      res.json("done");
     } catch (err) {
       res.status(400).json({ error: err.msg });
     }
