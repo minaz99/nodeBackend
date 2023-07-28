7e6cd4dfd1fefeb6465aa956c3accadceb905719
minaz99 <hanymina43@gmail.com>
2023-07-28 09:11:40 +0200

    trial i response to check for nulls

--- services/contract.service.js
+++ services/contract.service.js
@@ -120,7 +120,7 @@ const contract = {
       const newVideo = video !== null ? video : result.rows[0].video;
       const newContractStage =
         contractStage !== null ? contractStage : result.rows[0].contractStage;
-      const result2 = await db.query(
+      /* const result2 = await db.query(
         `UPDATE contracts SET eventLocation = $1, eventDate = $2, photographer = $3, video = $4, contractStage = $5, comments = $6 WHERE id = $7`,
         [
           newEventLocation,
@@ -131,8 +131,10 @@ const contract = {
           newComments,
           req.params.id,
         ]
+      );*/
+      res.json(
+        `eventLocation: ${newEventLocation}, eventDate: ${nenwEventDate}, photographer: ${newPhotographer}, video: ${newVideo}, contractStage: ${newContractStage}, comments: ${newComments} `
       );
-      res.json(`Contract has been updated`);
     } catch (err) {
       res.json({ error: err.msg });
     }
