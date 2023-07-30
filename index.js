e31305cde6063d03c5d9652944ff6f5b180b001e
minaz99 <hanymina43@gmail.com>
2023-07-30 22:09:33 +0200

    adding cors

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express.json());
+const cors = require("cors");
 
+app.use(
+  cors({
+    origin: "http:localhost:3000",
+  })
+);
 
 const userRouter = require("./routes/user.route");
 const contractRouter = require("./routes/contract.route");
