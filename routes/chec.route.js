748ac72b2d71c8fae3a2f749fb66ed9ba80c228a
minaz99 <hanymina43@gmail.com>
2023-07-30 19:36:23 +0200

    removed chec route (useless was for trial)

--- routes/chec.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const cs = require("../services/chec.service");
-
-router.get("/", cs.def);
-//router.get("/contracts", authToken, cs.contract);
-module.exports = router;
