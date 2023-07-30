c6d6260379bf7241256f639ee5fad07a3a5f803e
minaz99 <hanymina43@gmail.com>
2023-07-30 20:27:41 +0200

    changed endpoint for table header filters

--- routes/contract.route.js
+++ routes/contract.route.js
@@ -24,7 +24,7 @@ router.get(
   paymentService.getPaymentsInfoForContract
 );
 router.get(
-  "/tableHeaderFilters",
+  "/filter/tableHeaders",
   authToken,
   contractService.getTableHeaderFilters
 );
