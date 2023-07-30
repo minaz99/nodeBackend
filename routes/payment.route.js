8ce32beaacd0a4122ab574c825772ced3534b40f
minaz99 <hanymina43@gmail.com>
2023-07-30 19:39:05 +0200

    deleted payment route used inside contract route

--- routes/payment.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authToken = require("../authToken");
-const paymentService = require("../services/payment.service");
-
-router.get("/:id", authToken, paymentService.getPaymentsInfoForContract);
-router.post("/", authToken, paymentService.makePayment);
-
-module.exports = router;
