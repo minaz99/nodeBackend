794f00b5480dae096cce78e6aa10d9ac32fc8669
minaz99 <hanymina43@gmail.com>
2023-07-30 19:36:35 +0200

    removed trial route (useless was for trial)

--- routes/trial.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const trialService = require("../services/trial.service");
-
-//router.post("/login", userServices.login);
-router.get("/", trialService.contractsHello);
-
-module.exports = router;
