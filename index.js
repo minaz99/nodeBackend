a7bfa6a8ff83f2cb90bb402d45113b42080e958d
minaz99 <hanymina43@gmail.com>
2023-07-31 13:11:00 +0200

    3azama

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const cors = require("cors");
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 /*
 const userRouter = require("./routes/user.route");
 const contractRouter = require("./routes/contract.route");
