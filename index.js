7bb2b2ca55874aa535dd9b6f0da96ceed7d9053f
minaz99 <hanymina43@gmail.com>
2023-08-01 23:21:51 +0200

    adding contracts routes to the index file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require("express");
 const router = express.Router();
 const app = express();
 const cors = require("cors");
+const authToken = require("./authToken");
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -19,40 +20,52 @@ const contractRouter = require("./routes/contract.route");
 const componentRouter = require("./routes/component.route");
 const packageRouter = require("./routes/package.route");*/
 const userServices = require("./services/user.service");
-
-router.post("/login", userServices.login);
-router.post("/register", userServices.register);
-//app.post("/user/login", userServices.login);
-//app.post("/user/register", userServices.register);
+const contractService = require("./services/contract.service");
+const paymentService = require("./services/payment.service");
+//router.post("/login", userServices.login);
+//router.post("/register", userServices.register);
+app.post("/user/login", userServices.login);
+app.post("/user/register", userServices.register);
+app.get("/contracts/", authToken, contractService.getAllContracts);
+app.get("contracts/:id", authToken, contractService.getContractByID);
+app.get(
+  "/contracts/filter/filterType",
+  authToken,
+  contractService.getContractsByCriteria
+);
+app.get(
+  "/contracts/search/:bride",
+  authToken,
+  contractService.getContractByBride
+);
+app.get(
+  "/contracts/filter/multipleFilters",
+  authToken,
+  contractService.getContractsByMultipleFilters
+);
+app.get(
+  "/contracts/filter/calender",
+  authToken,
+  contractService.getContractsPerMonth
+);
+app.get(
+  "/contracts/:id/payments",
+  authToken,
+  paymentService.getPaymentsInfoForContract
+);
+app.get(
+  "/contracts/filter/tableHeaders",
+  authToken,
+  contractService.getTableHeaderFilters
+);
+app.post("/contracts/", authToken, contractService.createContract);
+app.post("/contracts/:id", authToken, contractService.updateContractDetails);
+app.post("/contracts/:id/payments", authToken, paymentService.makePayment);
 /*app.use("/user", userRouter);
 app.use("/contracts", contractRouter);
 app.use("/components", componentRouter);
 app.use("/packages", packageRouter);*/
-/*app.get("/", async (req, res) => {
-  res.json({ msg: "Hello there" });
-});
-app.post("/login", async (req, res) => {
-  try {
-    let { email, password } = req.body;
-    const user = { email: email };
-    const result = await db.query(`SELECT * FROM users where email = $1`, [
-      email,
-    ]);
 
-    if (result.rowCount > 0) {
-      bcrypt.compare(password, result.rows[0].password, (err, isMatch) => {
-        if (err) throw err;
-        if (isMatch) {
-          const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-          res.json({ accessToken: accessToken, name: result.rows[0].name });
-          //res.json({ name: result.rows[0].name });
-        }
-      });
-    } else res.status(404).json("User doesn't exist");
-  } catch (err) {
-    res.status(400).json({ error: err });
-  }
-});*/
 app.listen(process.env.PORT, () =>
   console.log("Server is running on port 5000")
 );
