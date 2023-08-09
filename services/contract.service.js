7786c10ee62eb9d856739038a7ab5fe596c92e7a
minaz99 <hanymina43@gmail.com>
2023-08-09 06:33:57 +0300

    ddd

--- services/contract.service.js
+++ services/contract.service.js
@@ -223,11 +223,11 @@ const contract = {
       let result3;
       let prevMonth = req.query.month;
       let nextMonth = parseInt(req.query.month);
-      let month = parseInt(req.query.month);
+      let month = req.query.month;
       let contractsMonthDetails = [];
       let prevYear = req.query.year;
       let nextYear = parseInt(req.query.year);
-      let year = parseInt(req.query.year);
+      //let year = parseInt(req.query.year);
       let daysInMonth = getDaysInMonth(req.query.year, req.query.month);
       let daysNeededFromPreviousMonth = new Date(
         `${req.query.year}/${req.query.month}/1`
