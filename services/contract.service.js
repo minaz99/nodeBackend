cac0de753bb584f228ad1206181023239cbedae0
minaz99 <hanymina43@gmail.com>
2023-08-10 01:23:51 +0300

    dsfds

--- services/contract.service.js
+++ services/contract.service.js
@@ -250,7 +250,7 @@ const contract = {
       } else daysInNextMonth = getDaysInMonth(nextYear, nextMonth + 1);
 
       if (daysNeededFromPreviousMonth !== 0) {
-        for (
+        /*   for (
           let i = daysInPrevMonth - daysNeededFromPreviousMonth + 1;
           i <= daysInPrevMonth;
           i++
@@ -294,14 +294,14 @@ const contract = {
               new Date(contract.eventdate).getDate() -
               1
           ].contracts.push(contract)
-        );
+        );*/
       } else {
         for (let i = 1; i <= daysInMonth; i++) {
           contractsMonthDetails.push({ day: i, contracts: [] });
         }
       }
 
-      result = await db.query(
+      /*  result = await db.query(
         `SELECT * FROM contracts where EXTRACT(MONTH FROM eventDate)=$1 AND EXTRACT(YEAR FROM eventDate)=$2 `,
         [parseInt(req.query.month), parseInt(req.query.year)]
       );
@@ -313,7 +313,7 @@ const contract = {
             1
         ].contracts.push(contract)
       );
-
+        */
       /*result.rows.forEach((contract) =>
         contractsMonthDetails[
           new Date(contract.eventdate).getDate()
