2099939b361fd3b280b8d99e2303a9fada562a61
minaz99 <hanymina43@gmail.com>
2023-08-10 01:34:26 +0300

    zdsf

--- services/contract.service.js
+++ services/contract.service.js
@@ -218,9 +218,9 @@ const contract = {
   },
   getContractsPerMonth: async (req, res) => {
     try {
-      let result1;
+      /*let result1;
       let result;
-      let result3;
+      let result3;*/
       /*let prevMonth = req.query.month;
       let nextMonth = parseInt(req.query.month);
       let month = parseInt(req.query.month);*/
@@ -334,9 +334,9 @@ const contract = {
         daysNeededFromNextMonth: daysNeededFromNextMonth,
         month: month,
         year: year,*/
-        result: result.rows,
+        /* result: result.rows,
         resul1: result1.rows,
-        result3: result3.rows,
+        result3: result3.rows,*/
       });
     } catch (err) {
       res.status(400).json({ error: err.msg });
