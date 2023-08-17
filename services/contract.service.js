24480187c3ddd983274962b71cbf0eb6ce044d13
minaz99 <hanymina43@gmail.com>
2023-08-18 02:45:24 +0300

    smdnf

--- services/contract.service.js
+++ services/contract.service.js
@@ -91,7 +91,7 @@ const contract = {
           createdBy,
         ]
       );
-      res.status(200).json({ contract: result.rows[0], id: result.rows[0].id });
+      res.status(200).json({ contract: result.rows });
     } catch (err) {
       res.status(400).json({ error: err.msg });
     }
